f2b98116a768484aee80b2493b4b985d3a7b6a04
Singhal <praveen.singhal@capgemini.com>
2017-03-10 19:22:05 +0530

    another commit

--- AcountService/src/main/java/com/accountservice/controller/AccountServiceController.java
+++ AcountService/src/main/java/com/accountservice/controller/AccountServiceController.java
@@ -1,13 +1,9 @@
 package com.accountservice.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
